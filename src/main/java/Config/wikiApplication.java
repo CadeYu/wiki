f8176488d54401092cc4bdea63b52d1f43de7e02
CadeYu <yu83612457@gmail.com>
2022-07-20 22:11:14 +0800

    开发hello 接口

--- src/main/java/Config/wikiApplication.java
+++ src/main/java/Config/wikiApplication.java
@@ -1,18 +1,14 @@
-package com.jiawa.wiki;
+package Config;
 
-import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@ComponentScan("com.jiawa")
 public class wikiApplication {
     private static final Logger LOG =
             LoggerFactory.getLogger(wikiApplication.class);
