215d06ce900b1c802c84ef1b796ae2c7007ec385
CadeYu <yu83612457@gmail.com>
2022-07-22 20:58:54 +0800

    增加ebook的封装返回类并对service层代码进行重构

--- src/main/java/com/jiawa/wiki/Controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/Controller/EbookController.java
@@ -1,10 +1,13 @@
 package com.jiawa.wiki.Controller;
 
 import com.jiawa.wiki.Domain.Ebook;
+import com.jiawa.wiki.Req.EbookReq;
 import com.jiawa.wiki.Resp.CommonResp;
+import com.jiawa.wiki.Resp.EbookResp;
 import com.jiawa.wiki.Service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -19,13 +22,14 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public CommonResp list(){
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq ebookReq){
+        List<EbookResp> ebookRespList = ebookService.list(ebookReq);
+        CommonResp<List<EbookResp>> CommonResp = new CommonResp<>();
 
-        CommonResp<List<Ebook>> resp = new CommonResp<List<Ebook>>();
+        CommonResp.setContent(ebookRespList);
 
-        resp.setContent(list);
+        return CommonResp;
 
-        return resp;
+
     }
 }
--- src/main/java/com/jiawa/wiki/Service/EbookService.java
+++ src/main/java/com/jiawa/wiki/Service/EbookService.java
@@ -1,10 +1,16 @@
 package com.jiawa.wiki.Service;
 
 import com.jiawa.wiki.Domain.Ebook;
+import com.jiawa.wiki.Domain.EbookExample;
 import com.jiawa.wiki.Mapper.EbookMapper;
+import com.jiawa.wiki.Req.EbookReq;
+import com.jiawa.wiki.Resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.management.relation.InvalidRelationTypeException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +19,29 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq ebookReq){
+        //模糊查询
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+
+          criteria.andNameLike("%" + ebookReq.getName() + "%");
+
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> reqList = new ArrayList<>();
+
+        for(Ebook ebook : ebookList){
+            EbookResp ebookResp = new EbookResp();
+
+            BeanUtils.copyProperties(ebook, ebookResp);
+
+            reqList.add(ebookResp);
+
+
+        }
+
+        return reqList;
+
     }
 
 }
