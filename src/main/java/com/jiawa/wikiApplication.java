0a7a517edf68ab600943bc6bbdf148d704efe0b6
CadeYu <yu83612457@gmail.com>
2022-07-22 16:59:04 +0800

    maven依赖更新

--- src/main/java/com/jiawa/wikiApplication.java
+++ src/main/java/com/jiawa/wikiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
