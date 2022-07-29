b485c001dfdb613d45b253d361e6c5283e02047c
CadeYu <yu83612457@gmail.com>
2022-07-29 17:31:11 +0800

    代码重构

--- src/main/java/com/jiawa/wiki/Config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/Config/SpringMvcConfig.java
@@ -1,23 +1,23 @@
-package com.jiawa.wiki.Config;
-
-import com.jiawa.wiki.Interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- *
- */
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-    LogInterceptor logInterceptor;
-
-
-    public void  addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor).
-                addPathPatterns("/**");// 针对所有的请求
-
-    }
-
-
-}
+//package com.jiawa.wiki.Config;
+//
+//import com.jiawa.wiki.Interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// *
+// */
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//    LogInterceptor logInterceptor;
+//
+//
+//    public void  addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor).
+//                addPathPatterns("/**");// 针对所有的请求
+//
+//    }
+//
+//
+//}
